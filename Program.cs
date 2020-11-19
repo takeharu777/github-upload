a822a73bc810caaef97c173722bdc40a8f3fe950
unknown <takeharu777@utake7.onmicrosoft.com>
2020-11-19 21:42:02 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace a_temp_proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
